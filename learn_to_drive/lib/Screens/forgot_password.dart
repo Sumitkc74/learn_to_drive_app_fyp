31b239b4e509803d48d7cbe5dedd64cf4af2bab0
Sumit KC <sumitkc74@gmail.com>
2023-01-29 23:49:54 +0545

    commit

--- learn_to_drive/lib/Screens/forgot_password.dart
+++ learn_to_drive/lib/Screens/forgot_password.dart
@@ -19,58 +19,58 @@ class _ForgotPasswordState extends State<ForgotPasswordScreen> {
       backgroundColor: const Color(0xFF303030),
       body: Stack(
         children: [                        
-            Container(
-              padding: const EdgeInsets.only(
-                right: 35, 
-                left: 35
-              ),
+          Container(
+            padding: const EdgeInsets.only(
+              right: 35, 
+              left: 35
+            ),
 
-              child: Column(
-                children: [
-                  Container(
-                    padding: const EdgeInsets.only(left: 0,top: 20, bottom: 20),
-                    child: const Text(
-                      'Get Email Verfication', 
-                      style:TextStyle(color: Colors.white, fontSize: 28)
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(left: 0,top: 20, bottom: 20),
+                  child: const Text(
+                    'Get Email Verfication', 
+                    style:TextStyle(color: Colors.white, fontSize: 28
                     )
+                  )
+                ),
+                const SizedBox(height: 25,),
+
+                const TextField(
+                  decoration: InputDecoration(
+                    filled: true, 
+                    fillColor: Colors.white,
+                    hintText: 'Enter verification code',
+                    border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(30))),
                   ),
-                  const SizedBox(height: 25,),
+                ),
+                const SizedBox( height: 20,),
 
-                  const TextField(
-                    decoration: InputDecoration(
-                      filled: true, 
-                      fillColor: Colors.white,
-                      hintText: 'Enter verification code',
-                      border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(30))),
-                    ),
+                Container(
+                  margin: const EdgeInsets.all(5),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFFFDE17),
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  const SizedBox( height: 20,),
 
-                  Container(
-                    margin: const EdgeInsets.all(5),
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: const Color(0xFFFFDE17),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-
-                    child:(
-                      TextButton(
-                        onPressed: () {}, 
-                        child: const Text(
-                          'Confirm',
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.white,
-                          )
+                  child:(
+                    TextButton(
+                      onPressed: () {}, 
+                      child: const Text(
+                        'Confirm',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
                         )
                       )
-                    ),
+                    )
                   ),
-                ],
-              )
-            ),
-          
+                ),
+              ],
+            )
+          ),     
         ], 
       ),
     );  
